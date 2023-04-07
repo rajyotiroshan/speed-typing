a9be55ed0263f09114d5a0521b6d6c2be89f9a11
rajan <rajyotiroshan@gmail.com>
2023-04-07 20:02:35 +0530

    timeReamining

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App(props) {
   const [text, setText] = useState("Hi there! start typing in the box");
   const [wc, setWC] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
   const updateText = (evt) => {
     setText(evt.target.value);
     countWord();
@@ -14,10 +16,19 @@ function App(props) {
     console.log(wc);
     setWC(wc);
   };
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <>
       <h1>Speed Typing Game</h1>
-      <h4>4:00</h4>
+      <h4>Time Remaining:: {timeRemaining}</h4>
       <textarea value={text} onChange={updateText} />
       <button>Start</button>
       <h1>Word count:: {wc} </h1>
