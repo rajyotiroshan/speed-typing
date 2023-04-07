d61efa670f1e4a33e255a08624c03a72e08a1891
rajan <rajyotiroshan@gmail.com>
2023-04-07 19:22:25 +0530

    app state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
+  const [text, setText] = useState("");
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
   return (
     <div>
       <h1>Speed Typing Game: How fastdo you type?</h1>
       {/**react normalizes the text area as input tag so self cosing tag */}
-      <textarea />
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h1>Word count ??</h1>
     </div>
