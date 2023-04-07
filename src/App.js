eb81fef3c205a53329f10cb018a1e4199774bb6e
rajan <rajyotiroshan@gmail.com>
2023-04-07 19:17:52 +0530

    state for text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App(props) {
-const text = "Hi there! start typing in the box";
+const [text, setText] = useState("Hi there! start typing in the box");
+
+const updateText = (evt)=>{
+  setText(evt.target.value);
+}
+
   return (
     <>
       <h1>Speed Typing Game</h1>
       <h4>4:00</h4>
-      <textarea value={text}/>
+      <textarea value={text} onChange={updateText}/>
       <button>Start</button>
       <h1>20</h1>
     </>
