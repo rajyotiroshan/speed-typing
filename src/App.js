c25e16004926b7d647e3bd74a967b41b8b537277
rajan <rajyotiroshan@gmail.com>
2023-04-07 19:33:53 +0530

    wordcount function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function App(props) {
-const [text, setText] = useState("Hi there! start typing in the box");
+  const [text, setText] = useState("Hi there! start typing in the box");
+  const [wc, setWC] = useState(0);
+  const updateText = (evt) => {
+    setText(evt.target.value);
+    countWord();
+  };
 
-const updateText = (evt)=>{
-  setText(evt.target.value);
-}
-
+  const countWord = () => {
+    console.log(text);
+    const wc = text.replace(/-/g, " ").trim().split(/\s+/g).length;
+    console.log(wc);
+    setWC(wc);
+  };
   return (
     <>
       <h1>Speed Typing Game</h1>
       <h4>4:00</h4>
-      <textarea value={text} onChange={updateText}/>
+      <textarea value={text} onChange={updateText} />
       <button>Start</button>
-      <h1>20</h1>
+      <h1>Word count:: {wc} </h1>
     </>
-  )
+  );
 }
 
 export default App;
