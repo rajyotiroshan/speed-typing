505a5d621a760555189983caaeb5cad0eda9fda4
rajan <rajyotiroshan@gmail.com>
2023-04-07 19:56:16 +0530

    timeReaming

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App(props) {
   const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
@@ -13,12 +15,18 @@ function App(props) {
     return filteredWord.length;
   }
 
+  useEffect(()=>{
+    setTimeout(() => {
+      setTimeRemaining(time=> time-1)
+    }, 1000);
+  }, [timeRemaining])
+
   return (
     <div>
       <h1>Speed Typing Game: How fastdo you type?</h1>
       {/**react normalizes the text area as input tag so self cosing tag */}
       <textarea onChange={handleChange} value={text} />
-      <h4>Time remaining: ???</h4>
+      <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count: {0}</h1>
     </div>
